5a1e16a42b9fe2efac8e7c140f67b81e499ec5cd
Chenger1 <exs2199@gmail.com>
2021-04-02 19:11:47 +0300

    Add index.html

--- kinoteatr/admin_panel/views.py
+++ kinoteatr/admin_panel/views.py
@@ -3,7 +3,7 @@ from django.views.generic import View
 
 
 class IndexAdmin(View):
-    template_name = 'base.html'
+    template_name = 'index.html'
 
     def get(self, request):
         return render(request, self.template_name)
