2b5b8cbb26ac16d9ab4ba7eca688c80db4bbb660
Chenger1 <exs2199@gmail.com>
2021-04-03 20:53:02 +0300

    Add User model and test for it

---
+++ kinoteatr/cinema/models/user.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils import timezone
+
+from cinema.models.managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    language_choices = [
+        (0, 'Русский'),
+        (1, 'Украинский')
+    ]
+    gender_choices = [
+        (0, 'Мужской'),
+        (1, 'Женский')
+    ]
+
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=50)
+
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    address = models.CharField(max_length=150, blank=True)
+    card_number = models.CharField(max_length=50, blank=True)
+    language = models.IntegerField(choices=language_choices, default=0)
+    gender = models.IntegerField(choices=gender_choices)
+    birthday_date = models.DateTimeField(blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone_number', 'username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
---
+++ kinoteatr/cinema/tests/test_models/test_user.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class UsersManagersTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(email='user@mail.com', username='user1', password='secure',
+                                        phone_number='123231321', gender=0)
+        self.assertEqual(user.email, 'user@mail.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        try:
+            self.assertIsNotNone(user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='')
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email='', password="secure")
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(email='superuser@mail.com', username='user1', password='secure',
+                                                    phone_number='123231321', gender=1)
+        self.assertEqual(admin_user.email, 'superuser@mail.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        try:
+            self.assertIsNotNone(admin_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email='superuser@mail.com', password='secure', is_superuser=False)
