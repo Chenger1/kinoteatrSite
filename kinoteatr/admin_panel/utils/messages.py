35c241c5de36bc742a347b08548aee636a52fd94
Chenger1 <exs2199@gmail.com>
2021-04-06 21:38:58 +0300

    Use messages django framework for redirect with errors

---
+++ kinoteatr/admin_panel/utils/messages.py
@@ -0,0 +1,8 @@
+from django.contrib import messages
+
+
+def beautify_error_messages(errors, request):
+    for error in errors:
+        for key, item in error.as_data().items():
+            for elem in item:
+                messages.error(request, f'{key}-{elem.message}')
