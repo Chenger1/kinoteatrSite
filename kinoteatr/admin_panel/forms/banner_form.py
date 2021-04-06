b1552d0b0ef3a9d925dc2fbcf1e9ae0a3335db10
Chenger1 <exs2199@gmail.com>
2021-04-06 21:37:17 +0300

    Add forms for OnTopBanner

--- kinoteatr/admin_panel/forms/banner_form.py
+++ kinoteatr/admin_panel/forms/banner_form.py
@@ -1,9 +1,26 @@
 from django import forms
 
-from cinema.models.banners import BackgroundImage
+from cinema.models.banners import BackgroundImage, OnTopBanner
+from cinema.models.gallery import OnTopBannerGallery
 
 
 class BackgroundImageForm(forms.ModelForm):
     class Meta:
         model = BackgroundImage
         fields = ('image', 'status')
+
+
+class OnTopBannerForm(forms.ModelForm):
+    class Meta:
+        model = OnTopBanner
+        fields = '__all__'
+
+
+class OnTopBannerGalleryInlineForm(forms.ModelForm):
+    class Meta:
+        model = OnTopBannerGallery
+        fields = ('image', 'url', 'text')
+
+
+OnTopBannerGalleryFormSet = forms.inlineformset_factory(OnTopBanner, OnTopBannerGallery,
+                                                        form=OnTopBannerGalleryInlineForm, extra=4)
