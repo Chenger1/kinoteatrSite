246bc18d4de833de0abc58175bec0a4a501aaa0f
Chenger1 <exs2199@gmail.com>
2021-04-07 22:54:04 +0300

    Update Seo model

--- kinoteatr/cinema/models/seo.py
+++ kinoteatr/cinema/models/seo.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Seo(models.Model):
-    seo_url = models.URLField()
-    seo_title = models.CharField(max_length=100)
-    seo_keywords = models.CharField(max_length=2000)
-    seo_description = models.TextField(max_length=5000)
+    seo_url = models.URLField(blank=True)
+    seo_title = models.CharField(max_length=100, blank=True)
+    seo_keywords = models.CharField(max_length=2000, blank=True)
+    seo_description = models.TextField(max_length=5000, blank=True)
 
     class Meta:
         db_table = 'seo'
