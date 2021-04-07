19cb74acb3c7163a696d92d7504568b5f69c4f07
Chenger1 <exs2199@gmail.com>
2021-04-07 22:21:59 +0300

    Add an additional beautify errors function

--- kinoteatr/admin_panel/utils/messages.py
+++ kinoteatr/admin_panel/utils/messages.py
@@ -6,3 +6,10 @@ def beautify_inline_error_messages(errors, request):
         for key, item in error.as_data().items():
             for elem in item:
                 messages.error(request, f'{key}-{elem.message}')
+
+
+def beautify_error_messages(errors):
+    result = []
+    for error, text in errors.items():
+        result.append(f'{error} - {text[0]}')
+    return result
