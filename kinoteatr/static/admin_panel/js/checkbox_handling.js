34623f754411335482c85fd63a5e0ab8a49f15bc
Chenger1 <exs2199@gmail.com>
2021-04-08 10:08:54 +0300

    Add comments for some js code

--- kinoteatr/static/admin_panel/js/checkbox_handling.js
+++ kinoteatr/static/admin_panel/js/checkbox_handling.js
@@ -1,9 +1,9 @@
-const radio1 = document.getElementById('radio1')
+const radio1 = document.getElementById('radio1') // get radio buttons
 const radio2 = document.getElementById('radio2')
 
-radio1.addEventListener('change', function (){
+radio1.addEventListener('change', function (){ // set listening for event - 'change'
     if(this.checked){
-        radio2.checked = false;
+        radio2.checked = false; // set radio = false if another radio button is checked
     }
 });
 radio2.addEventListener('change', function (){
--- kinoteatr/static/admin_panel/js/update_img_after_upload_static_form.js
+++ kinoteatr/static/admin_panel/js/update_img_after_upload_static_form.js
@@ -1,9 +1,9 @@
 function readURLSTATIC(input, image_id) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
+  if (input.files && input.files[0]) {// check if there are any files in input
+    var reader = new FileReader(); // initiate FileReader obj to get info about fle
 
     reader.onload = function(e) {
-      $('#'+image_id).attr('src', e.target.result);
+      $('#'+image_id).attr('src', e.target.result); // set new image for img tag by changing image src
     }
 
     reader.readAsDataURL(input.files[0]); // convert to base64 string
