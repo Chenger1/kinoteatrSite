5fc652b80597e3aea9ece4c0889e38fc3456c06f
chenger1 <exs2199@gmail.com>
2021-04-11 20:10:32 +0300

    Rewrite using Jquery

--- kinoteatr/static/admin_panel/js/checkbox_handling.js
+++ kinoteatr/static/admin_panel/js/checkbox_handling.js
@@ -1,13 +1,10 @@
-const radio1 = document.getElementById('radio1') // get radio buttons
-const radio2 = document.getElementById('radio2')
-
-radio1.addEventListener('change', function (){ // set listening for event - 'change'
-    if(this.checked){
-        radio2.checked = false; // set radio = false if another radio button is checked
-    }
-});
-radio2.addEventListener('change', function (){
-    if(this.checked){
-        radio1.checked = false;
-    }
+jQuery(document).ready(function($) {
+	let radio1 = $('.radio1')
+	let radio2 = $('.radio2')
+	radio1.click(function(){
+		radio2.checked = false;
+	})
+	radio2.click(function(){
+		radio1.checked = false;
+	})
 });
