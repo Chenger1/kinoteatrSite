0ec26be59a4e92c221eb2b24a970680bef3afac5
chenger1 <exs2199@gmail.com>
2021-04-11 20:12:35 +0300

    Add comments

--- kinoteatr/static/admin_panel/js/checkbox_handling.js
+++ kinoteatr/static/admin_panel/js/checkbox_handling.js
@@ -1,8 +1,8 @@
-jQuery(document).ready(function($) {
-	let radio1 = $('.radio1')
+jQuery(document).ready(function($) { // waiting for document to be loaded
+	let radio1 = $('.radio1') // get elements with selected classes
 	let radio2 = $('.radio2')
-	radio1.click(function(){
-		radio2.checked = false;
+	radio1.click(function(){ // set click handler for all elements
+		radio2.checked = false; // if element clicked, change 'checked' status of another to false
 	})
 	radio2.click(function(){
 		radio1.checked = false;
