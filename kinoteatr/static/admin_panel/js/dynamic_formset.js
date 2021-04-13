887b26a96927b244b8cc84f6837f3769bcd2d392
chenger1 <exs2199@gmail.com>
2021-04-13 22:16:20 +0300

    Update dynamic_formset.js. Fix bags, update functionality

--- kinoteatr/static/admin_panel/js/dynamic_formset.js
+++ kinoteatr/static/admin_panel/js/dynamic_formset.js
@@ -1,11 +1,29 @@
+function updateElementAttrForHidden(old_name){
+	reg = new RegExp('-[0-9]-'); // create new regular expression
+
+	matching_index = old_name.search(reg) + 1; // serach for matching
+	new_name = old_name.replace(old_name[matching_index], '0');
+	return new_name;
+}
+
 function cloneRow(selector, prefix, form_class, image_prefix){
-	let newElement = $(selector).clone(true); // copy element with his event handlers
 	let total = $('#id_'+prefix+'-TOTAL_FORMS').val(); // get total number if forms
+	let newElement;
+
+	if(total == 0){
+		newElement = $('.'+form_class+':first').css('display', 'block');
+	}else{
+		newElement = $(selector).clone(true); // copy element with his event handlers	
+	}
 
 	newElement.find(':input:not([type=button]):not([type=submit]):not([type=reset])').each(function(){ 
 	// find all inputs which are no 'button, submit, reset'
 	// replace the name and id of this inputs with new one to fit in to current forms row
-		let name = $(this).attr('name').replace('-'+(total-1)+'-', '-'+total +'-');
+		if(total == 0){
+			var name = updateElementAttrForHidden($(this).attr('name')); // update name if last form was hidden
+		}else{
+			var name = $(this).attr('name').replace('-'+(total-1)+'-', '-'+ total +'-');
+		}
 		let id = 'id_' + name;
 		$(this).attr({'name': name, 'id': id}).val('').removeAttr('checked'); 
 	});
@@ -13,16 +31,25 @@ function cloneRow(selector, prefix, form_class, image_prefix){
 	//find input with type 'file'. This input gets uploaded image in have 'onchange' attr to dynamic update image
 	// in template. On change functiion requires current form`s id. So it needs to update it
 	newElement.find("input[type='file']").each(function(){
-		let onchange_attr = $(this).attr('onchange').replace((total-1), total);
+		if(total == 0){
+			var onchange_attr = 'setPreview(this, 0, '+ "'"+image_prefix+"'"+')';
+		}else{
+			var onchange_attr = $(this).attr('onchange').replace((total-1), total);	
+		}
+		
 		$(this).attr({'onchange': onchange_attr});
 	})
 
 	newElement.find('label').each(function(){
 	// find label of the current form and repalce it 'for' attr
 	// to fit in with new form id
-		let forValue = $(this).attr('for');
+		var forValue = $(this).attr('for');
 		if (forValue){
-			forValue = forValue.replace('-' + (total-1)+'-', '-'+ total + '-');
+			if(total == 0){
+				forValue = forValue.replace('-' + (total-1)+'-', '-'+ '0' + '-');
+			}else{
+				forValue = forValue.replace('-' + (total-1)+'-', '-'+ total + '-');	
+			}
 			$(this).attr({'for': forValue});
 		};
 	});
@@ -30,9 +57,14 @@ function cloneRow(selector, prefix, form_class, image_prefix){
 	// preview img has special name to identify it from other form in formset. 
 	// after created new form, we have to update image name too. 
 	newElement.find('img').each(function(){
-		let nameValue = $(this).attr('name');
+		var nameValue = $(this).attr('name');
 		if (nameValue){
-			nameValue = nameValue.replace('_' + (total-1), '_'+ total)
+			if(total == 0){
+				nameValue = image_prefix + '_image_preview_0'
+			}else{
+				nameValue = nameValue.replace('_' + (total-1), '_'+ total);	
+			}
+			
 			$(this).attr({'name': nameValue});
 		}
 		$(this).attr('src', default_image); // set default image from static directory
@@ -70,16 +102,21 @@ function deleteForm(prefix, btn, className){
 	form_class = '.'+className;
 
 	let total = parseInt($('#id_'+prefix+'-TOTAL_FORMS').val()); // get integer(value) from given formset input - TOTAL FORMS
-	if(total >1){
+
+	if(total == 1){
+		btn.closest(form_class).css('display', 'none');
+		$('#id_'+prefix+'-TOTAL_FORMS').val(0);
+	}else{
 		btn.closest(form_class).remove(); // finds nearest form to delete
 		let forms = $(form_class);
-		$('#id_'+prefix+'-TOTAL_FORMS').val(forms.length); // update TOTAL_FORMS count according to new value
-
-		for(i=0; i<forms.length; i++){
-			$(forms.get(i)).find(':input').each(function(){ // Update All forms according to new value
-				updateElementIndex(this, prefix, i);
-			});
-		}
+	    $('#id_'+prefix+'-TOTAL_FORMS').val(forms.length);
 	}
-	return false;
-}
+	 // update TOTAL_FORMS count according to new value
+
+	for(i=0; i<forms.length; i++){
+		$(forms.get(i)).find(':input').each(function(){ // Update All forms according to new value
+			updateElementIndex(this, prefix, i);
+		});
+	};
+};
+
