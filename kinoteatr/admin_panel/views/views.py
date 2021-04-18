f0f6a2bbeeaf77ae1aaa1ce47d61f2027ab49d51
chenger1 <exs2199@gmail.com>
2021-04-18 23:18:59 +0300

    Add Login and Logout View for admin pages

--- kinoteatr/admin_panel/views/views.py
+++ kinoteatr/admin_panel/views/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
+
+from admin_panel.views.permission_mixin import AdminPermissionMixin
+from admin_panel.forms.authentication_form import LoginForm
 
 
-class IndexAdmin(View):
+class IndexAdmin(AdminPermissionMixin, View):
     template_name = 'index.html'
 
     def get(self, request):
         return render(request, self.template_name)
+
+
+class LoginUserAdmin(LoginView):
+    authentication_form = LoginForm
+    template_name = 'registration/login.html'
+    redirect_authenticated_user = True
+
+
+class LogoutAdmin(View):
+    def get(self, request):
+        logout(request)
+        return redirect('admin_panel:login_admin')
