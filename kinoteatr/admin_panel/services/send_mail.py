0c37bf9c4287b8969a393cc7cf4487488aeaa906
chenger1 <exs2199@gmail.com>
2021-04-23 18:00:01 +0300

    Add comments

--- kinoteatr/admin_panel/services/send_mail.py
+++ kinoteatr/admin_panel/services/send_mail.py
@@ -14,7 +14,8 @@ class Mail:
     def sending_mail(self):
         for user in self.users:
             html_message = render_to_string(self.template.get_content_path(), {'user': user})
-            plain_message = strip_tags(html_message)
-            from_email = settings.EMAIL_HOST_USER
-            to = user.email
+            # html content needs to be transformed to string to send by email protocols
+            plain_message = strip_tags(html_message)  # strip tags for less smart email clients
+            from_email = settings.EMAIL_HOST_USER  # set app email as a host
+            to = user.email  # users emails
             send_mail(self.subject, plain_message, from_email, [to], html_message=html_message)
--- kinoteatr/admin_panel/services/statistics.py
+++ kinoteatr/admin_panel/services/statistics.py
@@ -3,37 +3,48 @@ from cinema.models.user import User
 from cinema.models.session import Session, Ticket
 from cinema.models.cinema import CinemaHall
 
+from typing import List, Tuple, Dict
+
 
 class Statistic:
-    def get_movies(self):
-        return Movie.objects.filter(released=True).count()
+    def get_movies(self) -> List[Movie]:  # BUT, it is a QuerySet type
+        return Movie.objects.filter(released=True).count()  # counts only released movies
 
-    def get_last_10_movies(self):
+    def get_last_10_movies(self) -> List[Movie]:  # BUT, it is a QuerySet type
         return Movie.objects.all().order_by('-id')[:10]
 
-    def get_users(self):
+    def get_users(self) -> int:
         return User.objects.count()
 
-    def get_gender(self):
+    def get_gender(self) -> Tuple[int, int]:
         return User.objects.filter(gender=0).count(), User.objects.filter(gender=1).count()
 
-    def get_genre(self):
+    def get_genre(self) -> Dict[str, int]:
+        """
+            counts movies by each genre
+        """
         result = {}
         for genre in Movie.genre_choices:
             result[genre[1]] = Movie.objects.filter(genre=genre[0]).count()
         return result
 
-    def get_session(self):
+    def get_session(self) -> Dict[str, int]:
+        """
+        Count session my month
+        """
         result = {}
         for month in range(1, 13):
             result[month] = Session.objects.filter(session_datetime_start__month=month).count()
         return result
 
     def get_network_load(self):
+        """
+        Calculate how hard cinema network works
+        """
         total_seats = 0
         for hall in CinemaHall.objects.all():
-            total_seats += hall.seats_amount
+            total_seats += hall.seats_amount  # total number of seats
 
-        bought_tickets = Ticket.objects.filter(bought=True).count()
+        bought_tickets = Ticket.objects.filter(bought=True).count()  # how many tickets bought
         result = (bought_tickets*100)/total_seats
         return result
