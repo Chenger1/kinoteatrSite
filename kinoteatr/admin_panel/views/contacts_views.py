a19bfca7b41bad739301d30f942b71f3134b548a
chenger1 <exs2199@gmail.com>
2021-05-06 16:31:58 +0300

    Fix bug

--- kinoteatr/admin_panel/views/contacts_views.py
+++ kinoteatr/admin_panel/views/contacts_views.py
@@ -17,7 +17,9 @@ class ListContacts(AdminPermissionMixin, ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ListContacts, self).get_context_data(**kwargs)
-        context['seo'] = self.model.objects.filter(top_seo=True).first().seo
+        main_contact_seo = self.model.objects.filter(top_seo=True).first()
+        if main_contact_seo:
+            context['seo'] = main_contact_seo.seo
         return context
 
 
