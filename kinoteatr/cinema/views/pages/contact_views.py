6e84fb3521ac45c4d2fc39742126c18f48799953
chenger1 <exs2199@gmail.com>
2021-05-06 17:35:28 +0300

    Fix contacts filtering by active

--- kinoteatr/cinema/views/pages/contact_views.py
+++ kinoteatr/cinema/views/pages/contact_views.py
@@ -9,6 +9,10 @@ class DisplayContactPage(ListView):
     template_name = 'pages/contacts.html'
     context_object_name = 'contacts'
 
+    def get_queryset(self):
+        query_set = super().get_queryset()
+        return query_set.filter(status=True)
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(get_context_for_generic_views())
