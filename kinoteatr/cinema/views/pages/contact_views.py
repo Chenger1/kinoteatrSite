d73b9d42def5d7057382f89655a0826ce9249dff
chenger1 <exs2199@gmail.com>
2021-05-06 17:45:00 +0300

    Fix seo for contact list

--- kinoteatr/cinema/views/pages/contact_views.py
+++ kinoteatr/cinema/views/pages/contact_views.py
@@ -16,4 +16,7 @@ class DisplayContactPage(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(get_context_for_generic_views())
+        main_contact_seo = self.model.objects.filter(top_seo=True).first()
+        if main_contact_seo:
+            context['seo'] = main_contact_seo.seo
         return context
