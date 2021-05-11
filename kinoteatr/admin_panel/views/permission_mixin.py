025a6b34515ccb93b94743456bd020fe1c2e4f2c
chenger1 <exs2199@gmail.com>
2021-05-11 08:03:41 +0300

    Fix user perms checking

--- kinoteatr/admin_panel/views/permission_mixin.py
+++ kinoteatr/admin_panel/views/permission_mixin.py
@@ -3,6 +3,11 @@ from django.urls import reverse_lazy
 
 
 class AdminPermissionMixin(PermissionRequiredMixin):
-    permission_required = ('is_staff', )
+    permission_required = ('is_staff',)
     login_url = reverse_lazy('admin_panel:login_admin')
     permission_denied_message = 'У этого пользователя нет доступа к данной странице'
+
+    def has_permission(self):
+        if self.request.user.is_staff or self.request.user.is_superuser:
+            return True
+        return False
